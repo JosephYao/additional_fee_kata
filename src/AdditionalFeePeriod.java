4ab48fd578adc2207b414ed1ad92cebf36f2d20f
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-04-24 19:38:28 +0800

    date time later than start time but early than end time

--- src/AdditionalFeePeriod.java
+++ src/AdditionalFeePeriod.java
@@ -3,15 +3,21 @@ import java.time.LocalTime;
 
 public class AdditionalFeePeriod {
     private final LocalTime start;
+    private final LocalTime end;
 
     public AdditionalFeePeriod(LocalTime start, LocalTime end) {
         this.start = start;
+        this.end = end;
     }
 
     public void contains(LocalDateTime dateTime, Runnable ifTrue, Runnable ifFalse) {
-        if (dateTime.toLocalTime().equals(start))
+        if (timeOfDate(dateTime).equals(start) || timeOfDate(dateTime).isBefore(end))
             ifTrue.run();
         else
             ifFalse.run();
+    }
+
+    private LocalTime timeOfDate(LocalDateTime dateTime) {
+        return dateTime.toLocalTime();
     }
 }
--- src/TestAdditionalFeePeriod.java
+++ src/TestAdditionalFeePeriod.java
@@ -12,29 +12,48 @@ public class TestAdditionalFeePeriod {
     private static final int NOT_CONTAINED_HOUR = 19;
     private static final int ANY_MINUTE = 1;
     private static final int START_HOUR = 0;
-    private static final int START_MINUTE = 0;
-    AdditionalFeePeriod period = new AdditionalFeePeriod(LocalTime.of(0, 0), LocalTime.of(5, 0));
+    private static final int END_HOUR = START_HOUR + 5;
+    private static final int ZERO_MINUTE = 0;
     Runnable ifTrue = mock(Runnable.class);
     Runnable ifFalse = mock(Runnable.class);
 
     @Test
     public void not_in_period() {
-        period.contains(dateWithTime(NOT_CONTAINED_HOUR, ANY_MINUTE), ifTrue, ifFalse);
+        period(START_HOUR, END_HOUR).contains(dateWithTime(NOT_CONTAINED_HOUR, ANY_MINUTE), ifTrue, ifFalse);
 
-        verify(ifTrue, never()).run();
-        verify(ifFalse).run();
+        verifyIfFalseRun();
     }
 
     @Test
     public void date_time_exactly_on_start_time() {
-        period.contains(dateWithTime(START_HOUR, START_MINUTE), ifTrue, ifFalse);
+        period(START_HOUR, END_HOUR).contains(dateWithTime(START_HOUR, ZERO_MINUTE), ifTrue, ifFalse);
 
-        verify(ifTrue).run();
-        verify(ifFalse, never()).run();
+        verifyIfTrueRun();
+    }
+
+    @Test
+    public void date_time_later_than_start_time_but_early_than_end_time() {
+        period(START_HOUR, END_HOUR).contains(dateWithTime(START_HOUR + 1, ZERO_MINUTE), ifTrue, ifFalse);
+
+        verifyIfTrueRun();
+    }
+
+    private AdditionalFeePeriod period(int startHour, int endHour) {
+        return new AdditionalFeePeriod(LocalTime.of(startHour, ZERO_MINUTE), LocalTime.of(endHour, ZERO_MINUTE));
     }
 
     private LocalDateTime dateWithTime(int hour, int minute) {
         return LocalDateTime.of(2016, 4, 24, hour, minute);
+    }
+
+    private void verifyIfTrueRun() {
+        verify(ifTrue).run();
+        verify(ifFalse, never()).run();
+    }
+
+    private void verifyIfFalseRun() {
+        verify(ifTrue, never()).run();
+        verify(ifFalse).run();
     }
 
 }
