43d7865d72b684a1be66581706236a276db5dc62
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-04-24 19:21:42 +0800

    date time exactly on start time

--- src/AdditionalFeePeriod.java
+++ src/AdditionalFeePeriod.java
@@ -2,10 +2,16 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class AdditionalFeePeriod {
+    private final LocalTime start;
+
     public AdditionalFeePeriod(LocalTime start, LocalTime end) {
+        this.start = start;
     }
 
     public void contains(LocalDateTime dateTime, Runnable ifTrue, Runnable ifFalse) {
-        ifFalse.run();
+        if (dateTime.toLocalTime().equals(start))
+            ifTrue.run();
+        else
+            ifFalse.run();
     }
 }
--- src/TestAdditionalFeePeriod.java
+++ src/TestAdditionalFeePeriod.java
@@ -9,27 +9,32 @@ import static org.mockito.Mockito.verify;
 
 public class TestAdditionalFeePeriod {
 
+    private static final int NOT_CONTAINED_HOUR = 19;
+    private static final int ANY_MINUTE = 1;
+    private static final int START_HOUR = 0;
+    private static final int START_MINUTE = 0;
+    AdditionalFeePeriod period = new AdditionalFeePeriod(LocalTime.of(0, 0), LocalTime.of(5, 0));
+    Runnable ifTrue = mock(Runnable.class);
+    Runnable ifFalse = mock(Runnable.class);
+
     @Test
     public void not_in_period() {
-        AdditionalFeePeriod period = new AdditionalFeePeriod(LocalTime.of(0, 0), LocalTime.of(5, 0));
-        Runnable ifTrue = mock(Runnable.class);
-        Runnable ifFalse = mock(Runnable.class);
-
-        period.contains(LocalDateTime.of(2016, 4, 24, 19, 1), ifTrue, ifFalse);
+        period.contains(dateWithTime(NOT_CONTAINED_HOUR, ANY_MINUTE), ifTrue, ifFalse);
 
         verify(ifTrue, never()).run();
         verify(ifFalse).run();
     }
 
-//    @Test
-//    public void date_time_exactly_on_start_time() {
-//        AdditionalFeePeriod period = new AdditionalFeePeriod(LocalTime.of(0, 0), LocalTime.of(5, 0));
-//        Runnable ifTrue = mock(Runnable.class);
-//        Runnable ifFalse = mock(Runnable.class);
-//
-//        period.contains(LocalDateTime.of(2016, 4, 24, 0, 0), ifTrue, ifFalse);
-//
-//        verify(ifTrue).run();
-//        verify(ifFalse, never()).run();
-//    }
+    @Test
+    public void date_time_exactly_on_start_time() {
+        period.contains(dateWithTime(START_HOUR, START_MINUTE), ifTrue, ifFalse);
+
+        verify(ifTrue).run();
+        verify(ifFalse, never()).run();
+    }
+
+    private LocalDateTime dateWithTime(int hour, int minute) {
+        return LocalDateTime.of(2016, 4, 24, hour, minute);
+    }
+
 }
